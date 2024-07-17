7c77d303c6a4211b10dbc6012ce87585b65cd4ca
Steven Nielsen <steven.nielsen226@gmail.com>
2024-07-16 19:53:29 -0700

    added message variables and console.log the message

--- responsivevictory/day7/takeHomeChallenge/js/script.js
+++ responsivevictory/day7/takeHomeChallenge/js/script.js
@@ -5,10 +5,8 @@ let partnerName = "Jane Doe";
 let geographicLocation = "Vancouver";
 
 let jobTitle = "SAP coordinator";
-
-console.log(
-  `You will be a ${jobTitle} in ${geographicLocation}, and married to${partnerName} with ${numOfChildren} kids.`
-);
+let message1 = `You will be a ${jobTitle} in ${geographicLocation}, and married to${partnerName} with ${numOfChildren} kids.`;
+console.log(message1);
 
 let birthYear = 1992;
 
@@ -16,8 +14,8 @@ let year1 = 2030;
 
 let year2 = 2047;
 
-console.log(
-  `In the year 2030 i will be ${year1 - birthYear} and in 2047 i will be ${
-    year2 - birthYear
-  }`
-);
+let message2 = `In the year 2030 i will be ${
+  year1 - birthYear
+} and in 2047 i will be ${year2 - birthYear}`;
+
+console.log(message2);
